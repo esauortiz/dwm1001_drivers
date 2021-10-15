12af793f4f2cd4a383ce3e4e11004cb477719d7b
esauortiz <esauortiz97@gmail.com>
2021-10-15 14:04:32 +0200

    dwm1001_localizatio: using ekf instead of LS

--- scripts/dwm1001_localization.py
+++ scripts/dwm1001_localization.py
@@ -26,7 +26,7 @@ class AnchorSubscriber(object):
         rospy.Subscriber("/" + tag_id + "_tag_node/anchor_info_" + str(idx), AnchorInfo, self.callback)
 
 class LocationEngine(object):
-    def __init__(self, world_frame_id, tag0_id, tag1_id, n_anchors):
+    def __init__(self, world_frame_id, tag0_id, tag1_id, n_anchors, anchors_poses, ekf_kwargs):
         self.world_frame_id = world_frame_id
         # set anchor subscribers
         self.tag_coords = []
@@ -37,9 +37,15 @@ class LocationEngine(object):
                 self.anchor_subs_list.append(AnchorSubscriber(idx, tag_id))
         # set estimated coordinates pub
         self.estimated_coord_pub = rospy.Publisher("~tag_pose", PoseStamped, queue_size=10)
-        initial_pose = np.array([1.1259,3.0572,0.80672,0,0,0]) # manually from optitrack
-        
-        self.ekf = UWBfilter3D(ftype = 'EKF', x0 = initial_pose, dt = 1, std_acc = 0.01, std_rng = 0.05, landmarks = landmarks)
+        initial_pose = np.array([1.1259,3.0572,0.270672,0,0,0]) # manually from optitrack
+        self.old_ranges = self.compute_ranges(initial_pose[:3], anchors_poses)
+        self.ekf = UWBfilter3D(ftype = 'EKF', x0 = initial_pose, dt = ekf_kwargs['dt'], std_acc = ekf_kwargs['std_acc'], std_rng = ekf_kwargs['std_acc'], landmarks = anchors_poses)
+
+    def compute_ranges(self, tag_pose, anchors_poses):
+        ranges = []
+        for anchor_pose in anchors_poses:
+            ranges.append(np.linalg.norm((np.array(tag_pose),np.array(anchor_pose))))
+        return ranges
 
     def computeTagCoords(self, anchor_subs_updated):
         """
@@ -102,6 +108,7 @@ class LocationEngine(object):
     def loop(self, debug = False):
         # updated anchor subs list
         anchor_subs_updated = []
+        ranges = []
         for anchor_sub in self.anchor_subs_list:
             # check is subs have received new anchor info
             # also check if anchor status is True (i.e. anchor found)
@@ -111,12 +118,23 @@ class LocationEngine(object):
                 y = anchor_sub.anchor_info.position.y
                 z = anchor_sub.anchor_info.position.z
                 d = anchor_sub.anchor_info.distance
+                ranges.append(d)
                 if debug:
                     id = anchor_sub.anchor_info.id
                     print('anchor ' + str(id) + ' with coords (' + str(x) + ', ' + str(y) + ', ' + str(z) + ') and distance ' + str(d))
+            else:
+                ranges.append(-1)
+
+        # tag_coord computed through ekf
+        self.ekf.predict()
+        #if len(anchor_subs_updated) == 0:
+        #    ranges = self.old_ranges
+        self.ekf.update(ranges, niter = 100)
+        tag_coord = self.ekf.x
+        #self.old_ranges = ranges
 
         if len(anchor_subs_updated) >= 4:
-            tag_coord = self.computeTagCoords(anchor_subs_updated)
+            #tag_coord = self.computeTagCoords(anchor_subs_updated)
             ps = PoseStamped()
             ps.header.stamp = rospy.get_rostime()
             ps.header.frame_id = self.world_frame_id
@@ -147,9 +165,17 @@ if __name__ == '__main__':
     world_frame_id = str(rospy.get_param('~world_frame_id'))
     tag0_id = rospy.get_param('~tag0_id')
     tag1_id = rospy.get_param('~tag1_id')
-    
+    std_acc = float(rospy.get_param('~std_acc'))
+    std_rng = float(rospy.get_param('~std_rng'))
+    dt = float(rospy.get_param('~dt'))
+    ekf_kwargs = {'std_acc' : std_acc, 'std_rng' : std_rng, 'dt' : dt}
+
+    anchor_poses = np.empty((n_anchors, 3))
+    for i in range(n_anchors):
+        anchor_poses[i] = rospy.get_param('~anchor' + str(i) + '_coordinates')
+
     # location engine object
-    location_engine = LocationEngine(world_frame_id, tag0_id, tag1_id, n_anchors)
+    location_engine = LocationEngine(world_frame_id, tag0_id, tag1_id, n_anchors, anchor_poses, ekf_kwargs)
 
     while not rospy.is_shutdown():
         try:
