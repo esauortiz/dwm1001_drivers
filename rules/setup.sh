dab5cf422a06fc5bb35a968874b821b01d6005c6
esauortiz <esauortiz97@gmail.com>
2021-12-02 11:32:39 +0100

    ad script to setup dwm1001 modules dev rules

---
+++ rules/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo cp DW0038.rules /etc/udev/rules.d
+sudo cp DW009A.rules /etc/udev/rules.d
+sudo cp DW02D6.rules /etc/udev/rules.d
+sudo cp DW1632.rules /etc/udev/rules.d
+sudo cp DW2D9C.rules /etc/udev/rules.d
+sudo cp DW43EB.rules /etc/udev/rules.d
+sudo cp DW47FC.rules /etc/udev/rules.d
+sudo cp DW4806.rules /etc/udev/rules.d
+sudo cp DW4814.rules /etc/udev/rules.d
+sudo cp DW482E.rules /etc/udev/rules.d
+sudo cp DW4848.rules /etc/udev/rules.d
+sudo cp DW4984.rules /etc/udev/rules.d
+sudo cp dwm1001.rules /etc/udev/rules.d
+sudo udevadm control --reload-rules && udevadm trigger
